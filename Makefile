442576838469e16e34bb575dbc518fa755083b15
EHMD28 <ehmdiagne0530@gmail.com>
2025-03-17 11:34:40 -0400

    added read_rest and read_chars

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 tests: tests.c src/arachne.c
-	$(CC) -o tests.out tests.c src/arachne.c
+	$(CC) -o tests.out tests.c src/arachne.c -fsanitize=address
 	
--- tests.c
+++ tests.c
@@ -2,28 +2,35 @@
 #include <stdio.h>
 #include <string.h>
 
+void read_chars_example(void) {
+    puts("Read Characters Example:");
+    const char* str = "The quick brown fox jumps over the lazy dog";
+    ArachneString astr = arachne_new_str(str);
+    printf("Fist 10 characters: %s\n", arachne_read_chars(&astr, 10));
+    arachne_free(&astr);
+}
+
 void read_range_example(void) {
     const char* str = "Let's eat, Grandma";
     puts("Read Range Example:");
     printf("String: %s\n", str);
-    struct ArachneString first = arachne_new_range(str, 0, 9);
-    struct ArachneString second = arachne_new_range(str, 10, 18);
+    ArachneString first = arachne_new_range(str, 0, 9);
+    ArachneString second = arachne_new_range(str, 10, 18);
     printf("First: \"%s\"\n", arachne_get_range(&first));
     printf("Second: \"%s\"\n", arachne_get_range(&second));
-    printf("%s%s\n", arachne_get_range(&first), arachne_get_range(&second));
+    printf("Whole: %s%s\n", arachne_get_range(&first),
+           arachne_get_range(&second));
 }
 
 void read_words_example(void) {
     const char* planets = "My Very Educated Mother Just Served Us Noodles";
     const char* current = NULL;
     ArachneString astr = arachne_new_str(planets);
-
     puts("Read Words Example:");
     printf("String: \"%s\"\n", planets);
     while ((current = arachne_read_word(&astr)) != NULL) {
         puts(current);
     }
-
     arachne_free(&astr);
 }
 
@@ -41,12 +48,25 @@ void read_word_with_delimeter(void) {
     arachne_free(&astr);
 }
 
+void read_rest_example(void) {
+    puts("Read Rest Example:");
+    const char* str = "Mr. Jock, TV quiz PhD, bags few lynx";
+    ArachneString astr = arachne_new_str(str);
+    arachne_read_chars(&astr, 18);
+    printf("Rest of String: \"%s\"", arachne_read_rest(&astr));
+    arachne_free(&astr);
+}
+
 int main(void) {
+    puts("");
+    read_chars_example();
     puts("");
     read_range_example();
     puts("");
     read_words_example();
     puts("");
     read_word_with_delimeter();
+    puts("");
+    read_rest_example();
     puts("");
 }
